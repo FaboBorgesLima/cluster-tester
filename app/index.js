dd5da6c8eeb702c05743524778f1dfdd474ed39c
FaboBorgesLima <fabo.borges.lima@gmail.com>
2025-11-05 16:25:07 -0300

    feat: now cluster source /home/fabo/projects/cluster-tester/.venv/bin/activate

--- app/index.js
+++ app/index.js
@@ -1,85 +1,109 @@
 import axios from "axios";
 import express from "express";
+import os from "os";
+import cluster from "cluster";
 
-const app = express();
+const numCPUs = os.cpus().length;
 
-// ---------------------------- ENTRY POINT
-app.get("/", (req, res) => {
-    res.json({
-        message: "Welcome to the performance test service",
-        endpoints: [
-            { method: "GET", path: "/fibonacci/:n" },
-            { method: "GET", path: "/bubble-sort?n=<number>" },
-        ],
-    });
-});
+if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
 
-// ---------------------------- FIBONACCI
-const service = axios.create({ baseURL: process.env.APP_URL });
-
-app.get("/fibonacci/:n", async (req, res) => {
-    const n = +req.params.n;
-    const start = new Date();
-
-    if (n == 1 || n == 2) {
-        res.json({ fibonacci: 1, start: start, end: new Date() });
-        return;
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
 
-    const nMinusOneRequest = service.get(`/fibonacci/${n - 1}`);
-    const nMinusTwoRequest = service.get(`/fibonacci/${n - 2}`);
-
-    const fibonacci =
-        (await nMinusOneRequest).data.fibonacci +
-        (await nMinusTwoRequest).data.fibonacci;
-
-    res.json({
-        start: start,
-        fibonacci,
-        end: new Date(),
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
     });
-
-    return;
-});
-
-// ---------------------------- BUBBLE SORT
-app.get("/bubble-sort", (req, res) => {
-    const n = +req.query.n;
-
-    const arr = [];
-
-    const start = new Date();
-
-    for (let i = n; i > 0; i--) {
-        arr.push(i);
-    }
-
-    bubbleSort(arr);
-
-    const end = new Date();
-
-    res.json({ start: start, end: end });
-});
-
-/**
- *
- * @param {number[]} arr
- */
-function bubbleSort(arr) {
-    for (let i = arr.length; i > 0; i--)
-        for (let j = 1; j < i; j++)
-            if (arr[j] < arr[j - 1]) {
-                const swap = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = swap;
-            }
+} else {
+    console.log(`Worker ${process.pid} started`);
+    startServer();
 }
 
-// ---------------------------- SERVER
+function startServer() {
+    const PORT = process.env.PORT || 8080;
+    const app = bootstrap();
+    app.listen(PORT, () => {
+        console.log(`started at ${PORT}`);
+        console.log(`app url ${process.env.APP_URL}`);
+    });
+}
 
-const PORT = 8080;
+function bootstrap() {
+    const app = express();
 
-app.listen(PORT, () => {
-    console.log(`started at ${PORT}`);
-    console.log(`app url ${process.env.APP_URL}`);
-});
+    // ---------------------------- ENTRY POINT
+    app.get("/", (req, res) => {
+        res.json({
+            message: "Welcome to the performance test service",
+            endpoints: [
+                { method: "GET", path: "/fibonacci/:n" },
+                { method: "GET", path: "/bubble-sort?n=<number>" },
+            ],
+        });
+    });
+
+    // ---------------------------- FIBONACCI
+    const service = axios.create({ baseURL: process.env.APP_URL });
+
+    app.get("/fibonacci/:n", async (req, res) => {
+        const n = +req.params.n;
+        const start = new Date();
+
+        if (n == 1 || n == 2) {
+            res.json({ fibonacci: 1, start: start, end: new Date() });
+            return;
+        }
+
+        const nMinusOneRequest = service.get(`/fibonacci/${n - 1}`);
+        const nMinusTwoRequest = service.get(`/fibonacci/${n - 2}`);
+
+        const fibonacci =
+            (await nMinusOneRequest).data.fibonacci +
+            (await nMinusTwoRequest).data.fibonacci;
+
+        res.json({
+            start: start,
+            fibonacci,
+            end: new Date(),
+        });
+
+        return;
+    });
+
+    // ---------------------------- BUBBLE SORT
+    app.get("/bubble-sort", (req, res) => {
+        const n = +req.query.n;
+
+        const arr = [];
+
+        const start = new Date();
+
+        for (let i = n; i > 0; i--) {
+            arr.push(i);
+        }
+
+        bubbleSort(arr);
+
+        const end = new Date();
+
+        res.json({ start: start, end: end });
+    });
+
+    /**
+     *
+     * @param {number[]} arr
+     */
+    function bubbleSort(arr) {
+        for (let i = arr.length; i > 0; i--)
+            for (let j = 1; j < i; j++)
+                if (arr[j] < arr[j - 1]) {
+                    const swap = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = swap;
+                }
+    }
+
+    return app;
+}
